54d05f3945c09ff8bb7332a9c5fdefc0cc4cb344
Timur <tna290@gmail.com>
2018-10-17 15:04:27 +0500

    Изменен вывод книг

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container" style="margin-top:3rem;">
+    @foreach($books as $book)
+        <div class="row justify-content-center">
             {{-- <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -17,16 +17,17 @@
                     You are logged in!
                 </div>
             </div> --}}
-            @foreach($books as $book)
-            <div class="card" style="width: 18rem;">
+            
+            
+            <div class="card w-50" style="margin-bottom: 3rem;">
+                <div class="card-header">{{$book->name}}</div>
                 <div class="card-body">
-                  <h5 class="card-title">{{$book->name}}</h5>
+                  {{-- <h5 class="card-title"></h5> --}}
                   <h6 class="card-subtitle mb-2 text-muted">Автор: {{$book->author->author_name}}</h6>
                   <h6 class="card-subtitle mb-2 text-muted">Жанр: {{$book->genre->genre_name}}</h6>
                 </div>
               </div>
         </div>
-        @endforeach
-    </div>
+            @endforeach
 </div>
 @endsection
