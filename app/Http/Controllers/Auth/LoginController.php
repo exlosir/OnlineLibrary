3ba5d1a1d7e7938cfa1ee200fe115b34bb4c83b5
Exlosir <tna290@gmail.com>
2018-10-28 15:10:17 +0500

    переопределение поля авторизации

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,6 +37,10 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function username() {
+        return 'username';
+    }
+
     public function logout(Request $request)
     {
         $this->guard()->logout();
