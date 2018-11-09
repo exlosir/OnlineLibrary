5fc21de4338c69570b8d88bf25a8df99f1e9e969
Miramsy <LevMirMail43@gmail.com>
2018-11-09 12:34:00 +0500

    Изменил старницу Контакты

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,47 +3,59 @@
 @section('content')
 
 @if ($errors->any())
-    <div class="alert alert-danger">
-        @foreach ($errors->all() as $error)
-            {{ $error }} <br>
-        @endforeach
-    </div>
+<div class="alert alert-danger">
+    @foreach ($errors->all() as $error)
+    {{ $error }} <br>
+    @endforeach
+</div>
 @endif
 
 @if (\Session::has('success'))
-    <div class="alert alert-success text-center">
-        {!! \Session::get('success') !!}
-    </div>
+<div class="alert alert-success text-center">
+    {!! \Session::get('success') !!}
+</div>
 @endif
 
-    <div class="container" style="margin-top:3rem;">
-        <div class="row justify-content-center">
-        <h2>App name: {{ $settings->name }}</h2>
-        <h2>Adress: {{ $settings->adress }}</h2>
-        <h2>Phone: {{ $settings->phone }}</h2>
-        <h2>Work shedule: {{ $settings->work_shedule }}</h2>
-        </div>
+<div class="container" style="margin-top:3rem;">
+    <div class="row justify-content-center">
+        <address>
+            <h2>App name: {{ $settings->name }}</h2>
+            <h2>Adress: {{ $settings->adress }}</h2>
+            <h2>Phone: {{ $settings->phone }}</h2>
+            <h2>Work shedule: {{ $settings->work_shedule }}</h2>
+        </address>
     </div>
+</div>
 
-    <form action="/sendFeedback" method="POST">
-        {{ csrf_field() }}
-        <div class="form-group">
-            <label>От кого</label>
-        <input type="text" class="form-control" name='from'  placeholder="От кого">
-        </div>
-        <div class="form-group">
-            <label>Заголовок сообщения</label>
-            <input type="text" class="form-control" name='header'  placeholder="Заголовок сообщения">
-        </div>
-        <div class="form-group">
-            <label>Сообщение</label>
-            <input type="text" class="form-control" name='message'  placeholder="Сообщение">
-        </div>
-        <div class="form-group">
-            <label>E-Mail</label>
-            <input type="email" class="form-control" name='email'  placeholder="E-mail" >
-        </div>
-        <button type="submit" class="btn btn-primary">Записать</button>
-    </form>
+<div class="container">
+    <div class="col-md-4 offset-md-4">
+        <form action="/sendFeedback" method="POST">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label>Ваше имя</label>
+                <input type="text" class="form-control" name='from' placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label>E-Mail</label>
+                <input type="email" class="form-control" name='email' placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label>Тема сообщения</label>
+                <input type="text" class="form-control" name='header' placeholder="Заголовок сообщения">
+            </div>
+
+            <div class="form-group">
+                <label for="message">Сообщение</label>
+                <textarea class="form-control" rows="5" id="message"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-block">Отправить</button>
+
+        </form>
+    </div>
+</div>
 
 @endsection
