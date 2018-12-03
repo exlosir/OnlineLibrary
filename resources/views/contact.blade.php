521498700269df4bdadeb79d6e2a8962908f547f
Miramsy <LevMirMail43@gmail.com>
2018-12-03 12:33:24 +0500

    паследние правки по картачкам

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,48 +16,75 @@
 </div>
 @endif
 
-<div class="container" style="margin-top:3rem;">
+
+<div class="container mb-4">
     <div class="row justify-content-center">
-        <address>
-            <h2>App name: {{ $settings->name }}</h2>
-            <h2>Adress: {{ $settings->adress }}</h2>
-            <h2>Phone: {{ $settings->phone }}</h2>
-            <h2>Work shedule: {{ $settings->work_shedule }}</h2>
-        </address>
+        <div class="card w-50 text-center">
+            <div class="card-header text-center">
+                <h3>Контактная информация</h3>
+            </div>
+            <div class="card-body">
+                <address>
+                    <h2>App name: {{ $settings->name }}</h2>
+                    <h2>Adress: {{ $settings->adress }}</h2>
+                    <h2>Phone: {{ $settings->phone }}</h2>
+                    <h2>Work shedule: {{ $settings->work_shedule }}</h2>
+                </address>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="col-md-6 offset-md-3 mb-4">
-        <h2 class="text-center">Здесь вы можете оставить сообщение администрации</h2>
-        <form action="/sendFeedback" method="POST">
-            {{ csrf_field() }}
-
-            <div class="form-group">
-                <label>Ваше имя</label>
-                <input type="text" class="form-control" name='from' placeholder="">
+<div class="container mb-4">
+    <div class="row justify-content-center">
+        <div class="card w-50">
+            <div class="card-header text-center">
+                <h2 class="text-center">Здесь вы можете оставить сообщение администрации</h2>
             </div>
+            <div class="card-body">
+                <form action="/sendFeedback" method="POST">
+                    {{ csrf_field() }}
 
-            <div class="form-group">
-                <label>E-Mail</label>
-                <input type="email" class="form-control" name='email' placeholder="">
+                    <div class="form-group">
+                        <label>Ваше имя</label>
+                        <input type="text" class="form-control" name='from' placeholder="">
+                    </div>
+
+                    <div class="form-group">
+                        <label>E-Mail</label>
+                        <input type="email" class="form-control" name='email' placeholder="">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Тема сообщения</label>
+                        <input type="text" class="form-control" name='header' placeholder="Заголовок сообщения">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="message">Сообщение</label>
+                        <textarea class="form-control" rows="5" id="message"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block">Отправить</button>
+                </form>
             </div>
-
-            <div class="form-group">
-                <label>Тема сообщения</label>
-                <input type="text" class="form-control" name='header' placeholder="Заголовок сообщения">
-            </div>
-
-            <div class="form-group">
-                <label for="message">Сообщение</label>
-                <textarea class="form-control" rows="5" id="message"></textarea>
-            </div>
-
-            <button type="submit" class="btn btn-primary btn-block">Отправить</button>
-        </form>
+        </div>
     </div>
 </div>
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2303.9339068826043!2d55.966612951121334!3d54.72837738019514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43d93a47ee1253d3%3A0xa2c621b9591ac95e!2z0YPQuy4g0JrQuNGA0L7QstCwLCA2NSwg0KPRhNCwLCDQoNC10YHQvy4g0JHQsNGI0LrQvtGA0YLQvtGB0YLQsNC9LCA0NTAwMDU!5e0!3m2!1sru!2sru!4v1543814984258" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div class="container mb-4">
+    <div class="row justify-content-center">
+        <div class="card w-75 h-75 text-center">
+            <div class="card-header">
+                <h2>Схема проезда</h2>
+            </div>
+            <div class="card-body">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2303.9339068826043!2d55.966612951121334!3d54.72837738019514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43d93a47ee1253d3%3A0xa2c621b9591ac95e!2z0YPQuy4g0JrQuNGA0L7QstCwLCA2NSwg0KPRhNCwLCDQoNC10YHQvy4g0JHQsNGI0LrQvtGA0YLQvtGB0YLQsNC9LCA0NTAwMDU!5e0!3m2!1sru!2sru!4v1543814984258"
+                    width="750" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -3,26 +3,31 @@
 @section('content')
 
 @if (\Session::has('error'))
-    <div class="alert alert-danger text-center">
-        {!! \Session::get('error') !!}
-    </div>
+<div class="alert alert-danger text-center">
+    {!! \Session::get('error') !!}
+</div>
 @endif
 
 @if (\Session::has('success'))
-    <div class="alert alert-success text-center">
-        {!! \Session::get('success') !!}
-    </div>
+<div class="alert alert-success text-center">
+    {!! \Session::get('success') !!}
+</div>
 @endif
 
-    <div class="container" style="margin-top:3rem;">
-        {{-- <div class="row justify-content-center"> --}}
-            <h2>Страница избранных книг</h2>
-            @foreach($favorite as $item)
+
+<div class="container mb-4">
+    <div class="row justify-content-center">
+        <div class="card w-75 bg-light">
+            <div class="card-header text-center">
+                <h2>Страница избранных книг</h2>
+            </div>
+            <div class="card-body">
+                @foreach($favorite as $item)
                 <div class="row justify-content-center">
-                    <div class="card w-50" style="margin-bottom: 3rem;">
+                    <div class="card w-75" style="margin-bottom: 3rem;">
                         <div class="card-header">
                             <div class="row">
-                                <div class="col-5 text-center"><b> {{$item->name}}</b></div>
+                                <div class="col-5 text-center" style="margin-top:0.5rem;"><b> {{$item->name}}</b></div>
                                 <div class="col-7 text-right">
                                     <form action="/favorite/delete/{{$item->id}}" method="POST">
                                         {{ csrf_field() }}
@@ -33,24 +38,65 @@
                             </div>
                         </div>
                         <div class="card-body clearfix">
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="{{$item->link_for_img}}" class="card-img">
+                            <div class="row">
+                                <div class="col-3">
+                                    <img src="{{$item->link_for_img}}" class="card-img">
+                                </div>
+                                <div class="col-9">
+                                    <h6 class=" mb-2"><b> Автор: </b>{{$item->author->author_name}}</h6>
+                                    <h6 class=" mb-2"><b>Жанр: </b>{{$item->genre->genre_name}}</h6>
+                                    <h6 class=" mb-2"><b>Год написания: </b>{{$item->year}}</h6>
+                                </div>
                             </div>
-                            <div class="col-9">
-                                <h6 class=" mb-2"><b> Автор: </b>{{$item->author->author_name}}</h6>
-                                <h6 class=" mb-2"><b>Жанр: </b>{{$item->genre->genre_name}}</h6>
-                                <h6 class=" mb-2"><b>Год написания: </b>{{$item->year}}</h6>
-                            </div>
-                        </div>
                         </div>
                         <div class="card-footer">
-                                <a href="/catalog/{{$item->id}}" class="btn btn-success">Подробнее</a>
+                            <a href="/catalog/{{$item->id}}" class="btn btn-success">Подробнее</a>
                         </div>
                     </div>
                 </div>
-    @endforeach
-        {{-- </div> --}}
+                @endforeach
+            </div>
+        </div>
     </div>
+</div>
+
+<!-- <div class="container" style="margin-top:3rem;">
+    {{-- <div class="row justify-content-center"> --}}
+        <h2>Страница избранных книг</h2>
+        @foreach($favorite as $item)
+        <div class="row justify-content-center">
+            <div class="card w-50" style="margin-bottom: 3rem;">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-5 text-center"><b> {{$item->name}}</b></div>
+                        <div class="col-7 text-right">
+                            <form action="/favorite/delete/{{$item->id}}" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button class="btn btn-danger">Удалить из избранного</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body clearfix">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="{{$item->link_for_img}}" class="card-img">
+                        </div>
+                        <div class="col-9">
+                            <h6 class=" mb-2"><b> Автор: </b>{{$item->author->author_name}}</h6>
+                            <h6 class=" mb-2"><b>Жанр: </b>{{$item->genre->genre_name}}</h6>
+                            <h6 class=" mb-2"><b>Год написания: </b>{{$item->year}}</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="/catalog/{{$item->id}}" class="btn btn-success">Подробнее</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        {{-- </div> --}}
+</div> -->
 
 @endsection
